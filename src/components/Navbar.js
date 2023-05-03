6d05f856590cdd6954b5f3bc45877faabefed9cf
nossherlock <anahar1@asu.edu>
2023-05-03 00:53:47 -0700

    Navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,13 +19,7 @@ const Navbar = ({ onLogout }) => {
         <div className='menu-icon' onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
         </div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <li className='nav-item'>
-            <Link to='/lighthall-challenge-4/home' className='nav-links' onClick={closeMobileMenu}>
-              Home
-            </Link>
-          </li>
-          
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>          
           <li className='nav-item'>
             <Link
               to='/lighthall-challenge-4/invite'
